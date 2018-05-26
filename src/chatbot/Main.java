42ab68f8d7b31c8db84b7b1fa81b8793d5e5d576
Bany Alvarado <balvarado@plusti.com>
2018-05-25 23:16:20 -0600

    main

--- src/chatbot/Main.java
+++ src/chatbot/Main.java
@@ -15,11 +15,13 @@ import java.util.*;
 public class Main {
 
     String[] typeBears = {"pardo", "panda", "polar", "malayo", "americano", "asiatico"};
-    String[] adjetiveType = {"mostrar", "ver", "visualizar", "curiosidad", "tienen", "estan", "tipos", "tiene", "busco", "veo", "hay", "ofrecerme", "ofrecen"};
+    String[] adjetiveType = {"mostrar", "ver", "visualizar", "curiosidad", "tienen", "estan", "tipos", "tiene", "busco", "veo", "hay", "ofrecerme", "ofrecen", "osos", "peluches"};
     String[] typeAction = {"donde", "comprar", "quiero", "necesito", "compro"};
     String[] typeInterest = {"consigo", "encuentro", "interesa"};
-    String[] typeGreeting = {"hola", "buenas", "chatbot", "buenas tardes", "buenas noches", "ola", "buena noche", "buena tarde", "buenos dias", "buen dia"};
+    String[] typeGreeting = {"hola", "buenas", "buena", "chatbot", "buenas tardes", "buenas noches", "ola", "buena noche", "buena tarde", "buenos dias", "buen dia"};
     String[] typePayment = {"pago", "dinero", "deposito", "transferencia", "criptomoneda", "transferencias"};
+    //Preguntas
+    String[] questions = {"cuanto", "cuesta", "que", "valor", "tiene", "a", "precio"};
 
     private String clearSentence;
     private Set<String> resultBears = new HashSet<>();
@@ -28,10 +30,11 @@ public class Main {
     private Set<String> resultTypeGreeting = new HashSet<>();
     private Set<String> resultTypeInterest = new HashSet<>();
     private Set<String> resultTypePayment = new HashSet<>();
+    private Set<String> resultQuestion = new HashSet<>();
     private Scanner scan = new Scanner(System.in);
     private String userResponse;
 
-    public void executeSentence(String sentence) {
+    public String executeSentence(String sentence) {
 
         clearSentence = clearSentence(sentence.toLowerCase());
 
@@ -42,7 +45,10 @@ public class Main {
         checkForWordsWithInterest(clearSentence, typeInterest);
         checkForWordsWithPayment(clearSentence, typePayment);
 
-        detectUserBehavior();
+        //pregunta
+        checkQuestion(clearSentence, questions);
+
+        return detectUserBehavior();
     }
 
     public String clearSentence(String words) {
@@ -54,6 +60,18 @@ public class Main {
         return words;
     }
 
+    public void checkQuestion(String sentence, String[] words) {
+        String[] wordsInSentence = sentence.split(" ");
+
+        for (String word : wordsInSentence) {
+            for (String word2 : words) {
+                if (word.equals(word2)) {
+                    resultQuestion.add(word);
+                }
+            }
+        }
+    }
+
     public void checkForWordsWithGreeting(String sentence, String[] words) {
         String[] wordsInSentence = sentence.split(" ");
 
@@ -61,7 +79,7 @@ public class Main {
             for (String word2 : words) {
                 if (word.equals(word2)) {
                     resultBears.add(word);
-                    break;
+                    // break;
                 }
             }
         }
@@ -73,7 +91,7 @@ public class Main {
             for (String word2 : words) {
                 if (word.equals(word2)) {
                     resultAdjetiveType.add(word);
-                    break;
+                    // break;
                 }
             }
         }
@@ -86,7 +104,7 @@ public class Main {
             for (String word2 : words) {
                 if (word.equals(word2)) {
                     resultTypeActions.add(word);
-                    break;
+                    //  break;
                 }
             }
         }
@@ -98,7 +116,7 @@ public class Main {
             for (String word2 : words) {
                 if (word.equals(word2)) {
                     resultTypeGreeting.add(word);
-                    break;
+                    // break;
                 }
             }
         }
@@ -110,7 +128,7 @@ public class Main {
             for (String word2 : words) {
                 if (word.equals(word2)) {
                     resultTypeInterest.add(word);
-                    break;
+                    //break;
                 }
             }
         }
@@ -122,71 +140,46 @@ public class Main {
             for (String word2 : words) {
                 if (word.equals(word2)) {
                     resultTypePayment.add(word);
-                    break;
+                    // break;
                 }
             }
         }
     }
 
-    public void detectUserBehavior() {
+    public String detectUserBehavior() {
+        String response = "";
+        int typeIntention = geIntention();
 
-        int typeIntention = 0;
-
-        for (String bears : resultBears) {
-            typeIntention = 1;
-            //System.out.println("Tipos de osos que pregunta: " + bears);
+        if (typeIntention != 7) {
+            switch (typeIntention) {
+                case 0:
+                   response= "No entiendo!";
+                    break;
+                case 1:
+                    break;
+                case 2:
+                   response = "Los osos que tenemos disponibles son los siguientes: pardo,panda,polar,malayo,americano,asiatico";
+                    break;
+                case 3:
+                    response ="Que oso de peluche deseas comprar?";
+                    break;
+                case 4:
+                    response = "Mucho gusto estimado usuario.\nEn que puedo ayudarlo?";
+                    break;
+                case 5:
+                   response = "Puedes conseguir los osos de peluche por aca";
+                    break;
+                case 6:
+                    response = "Aceptamos todos los metodos de pago incluso criptomonedas";
+                    break;
+            }
+        } else {
+            System.out.println("pregunta");
         }
 
-        for (String adjetive : resultAdjetiveType) {
-            typeIntention = 2;
-            //System.out.println("Tipos de adjetivos: " + adjetive);
-        }
-
-        for (String action : resultTypeActions) {
-            typeIntention = 3;
-            //System.out.println("Tipos de acciones: " + action);
-        }
-
-        for (String greeting : resultTypeGreeting) {
-            typeIntention = 4;
-            //System.out.println("Tipos de saludos: " + greeting);
-        }
-
-        for (String greeting : resultTypeInterest) {
-            typeIntention = 5;
-            //System.out.println("Tipos de saludos: " + greeting);
-        }
-
-        for (String greeting : resultTypePayment) {
-            typeIntention = 6;
-            //System.out.println("Tipos de saludos: " + greeting);
-        }
-
-        switch (typeIntention) {
-            case 0:
-                System.out.println("No entiendo!");
-                break;
-            case 1:
-                break;
-            case 2:
-                System.out.println("Los osos que tenemos disponibles son los siguientes: ");
-                System.out.println("pardo,panda,polar,malayo,americano,asiatico");
-                break;
-            case 3:
-                System.out.println("Que oso de peluche deseas comprar?");
-                break;
-            case 4:
-                System.out.println("Buenas tardes estimado usuario.\nEn que puedo ayudarlo?");
-                break;
-            case 5:
-                System.out.println("Puedes conseguir los osos de peluche por aca");
-                break;
-            case 6:
-                System.out.println("Aceptamos todos los metodos de pago incluso criptomonedas");
-                break;
-        }
         clearLastValues();
-       // waitResponse(); utilizar solamente en consola
+        return response;
+        //waitResponse();// utilizar solamente en consola
     }
 
     public void clearLastValues() {
@@ -196,6 +189,52 @@ public class Main {
         resultTypeGreeting = new HashSet<>();
         resultTypeInterest = new HashSet<>();
         resultTypePayment = new HashSet<>();
+        resultQuestion = new HashSet<>();
+    }
+
+    public int geIntention() {
+        int intention = 0;
+        int max = 0;
+        List<Integer> t = new ArrayList<Integer>();
+        t.add(resultBears.size());
+        t.add(resultAdjetiveType.size());
+        t.add(resultTypeActions.size());
+        t.add(resultTypeGreeting.size());
+        t.add(resultTypeInterest.size());
+        t.add(resultTypePayment.size());
+        t.add(resultQuestion.size());
+
+        for (int i = 0; i < t.size(); i++) {
+            if (t.get(i) > max) {
+                max = i;
+            }
+        }
+
+        switch (max) {
+            case 0:
+                intention = 1;
+                break;
+            case 1:
+                intention = 2;
+                break;
+            case 2:
+                intention = 3;
+                break;
+            case 3:
+                intention = 4;
+                break;
+            case 4:
+                intention = 5;
+                break;
+            case 5:
+                intention = 6;
+                break;
+            case 6:
+                intention = 7;
+                break;
+        }
+
+        return intention;
     }
 
     public void waitResponse() {
